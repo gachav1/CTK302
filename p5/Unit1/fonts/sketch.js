6813aad18ef7a78b79a4658c4f4e8cc58181d071
gachav1 <gachav1@ilstu.edu>
2020-09-01 11:46:55 -0500

    font test

---
+++ p5/Unit1/fonts/sketch.js
@@ -0,0 +1,19 @@
+var f ;
+
+function setup() {
+  createCanvas(800, 800);
+
+  f = loadFont("assets/bat.ttf")
+  textAlign(CENTER);
+
+}
+
+function draw() {
+  background(100) ;
+
+  textFont(f);
+  textSize(60);
+
+  text("hello", width/2, height/2);
+
+}
